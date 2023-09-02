8d361eba25f8586d0ae1771f69ee06ea786c7a5e
bsx <BsxHomu@outlook.com>
2023-09-02 20:57:38 +0800

    feat: add img

--- src/views/homePage/index.tsx
+++ src/views/homePage/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import Homepage from '@/assets/homepage.png'
 import styles from './index.module.scss'
 //导航为空时自动跳转
 const HomePage = () => {
   return (
-    <div className={styles.root}>HomePage</div>
+    <div className={styles.root}>
+      <img src={Homepage}/>
+    </div>
   )
 }
 
